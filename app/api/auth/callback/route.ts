48a363232a6b24a784e63d8a28ffaaa42ee26fb4
imran farooq <imran_farooq7@hotmail.com>
2025-12-20 22:03:09 +0500

    auth callback route added

---
+++ app/api/auth/callback/route.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // 'next' allows you to redirect the user to a specific page after login
+  const next = searchParams.get("next") ?? "/";
+
+  if (code) {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  // Handle errors (e.g., redirect to an error page)
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
