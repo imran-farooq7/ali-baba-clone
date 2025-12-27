b629fe712620c7c91490afc0cc763642cf276231
imran farooq <imran_farooq7@hotmail.com>
2025-12-27 23:13:34 +0500

    brand proposal details page added

---
+++ app/brand/proposals/[id]/page.tsx
@@ -0,0 +1,19 @@
+// app/(brand)/proposals/[id]/page.tsx
+import ProposalDetail from "@/components/proposals/proposal-details";
+import { getCurrentUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function BrandProposalDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const user = await getCurrentUser();
+  if (!user || user.type !== "BRAND") redirect("/auth/login");
+
+  return (
+    <BrandDashboardLayout user={user}>
+      <ProposalDetail />
+    </BrandDashboardLayout>
+  );
+}
